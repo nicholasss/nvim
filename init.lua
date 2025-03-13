8be2e72f5ab359e2a5eeb27f97f3e1d85ee4d572
Nicholas <nicholaszingleman@gmail.com>
2025-03-13 15:38:35 -0400

    updated; r

--- init.lua
+++ init.lua
@@ -388,7 +388,7 @@ require('lazy').setup({
         },
       }
 
-      require('mason').setup()
+      -- require('mason').setup()
 
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
